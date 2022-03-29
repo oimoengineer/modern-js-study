a49f5ae7b9d0898df0a96273d11eaee6a7fcf1f8
Nomi <79703777+oimoengineer@users.noreply.github.com>
2022-03-29 22:06:25 +0900

    モダンJSについての学習②

--- src/index.js
+++ src/index.js
@@ -35,6 +35,7 @@
  */
 
 // const name = "imoco";
+
 // const age = 21;
 // //  [My name is imoco.  I'm 21 years old.]
 
@@ -76,6 +77,7 @@
 // const myProfile = {
 //   name: "imoco",
 //   age: 21
+
 // };
 // const message1 = `My name is ${myProfile.name}. I'm ${myProfile.age}.`;
 // console.log(message1);
@@ -93,6 +95,65 @@
 /**
  * デフォルト値、引数など
  */
-const sayHello = (name = "guest") => console.log(`Hello!${name}!`);
-sayHello("imoco");
-sayHello();
+// const sayHello = (name = "guest") => console.log(`Hello!${name}!`);
+
+// sayHello("imoco");
+
+// sayHello();
+
+/**
+ * スプレッド構文　...
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる ..arr3を使用することで残りの3つの数値をまとめて受け取っている
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr6);
+// console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// ＝でコピーすると参照渡しになってしまう
+// const arr8 = arr4;
+// console.log(arr8);
+// arr8[0] = 100;
+// console.log(arr8);
+// console.log(arr4);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ？ 条件がtrueの時 : 条件がtrueの時
+// const val1 = 1 > 0 ? "It's true." : "It's false.";
+// console.log(val1);
+
+// 実装サンプル1
+// const num = '1300';
+
+// const formattedNum  = typeof num === 'number' ? num.toLocaleString() : 'Please number';
+// console.log(formattedNum);
+
+// 実装サンプル2
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? 'over 100' : 'OK';
+// }
+// console.log(checkSum(50, 40));
